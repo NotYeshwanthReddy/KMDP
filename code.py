ef922e86cf84a4eec0764e03abcca6b90ca23aaf
Not-Yeshwanth-Reddy <peddamalluyeshwanth1999@gmail.com>
2019-10-23 18:50:04 +0530

    Single Product Based Greedy Algorithm is working.

--- code.py
+++ code.py
@@ -13,8 +13,8 @@ File Type	: Python
 
 
 import pandas as pd
-import random
 import numpy as np
+import heapq
 
 
 def create_SBS(fileLocation = "car/car.data"):
@@ -59,12 +59,81 @@ def create_SBS(fileLocation = "car/car.data"):
 	print("Saving to file...")
 	pd.DataFrame(data, dtype=np.int8).to_csv("data_process/"+fileName+".csv", header=None, index=None)
 	pd.DataFrame(SBS, dtype=np.int8).to_csv("data_process/"+fileName+"_SBS.csv", header=None, index=None)
-
+	return SBS
 
 def read_SBS(fileLocation):
 	SBS = pd.read_csv(fileLocation, header=None)
 	return SBS
 
 
-# create_SBS("car/car.data")
-SBS = read_SBS("data_process/car_SBS.csv")
+
+def N_Calc():
+	Nsbs = [[0]*2]*w
+	for i in range(w):
+		EP_count=0
+		CP_count=0
+		for j in range(0, (h*30)//100):
+			if SBS[j][i]==1:
+				EP_count+=1
+		for j in range((h*30)//100, h):
+			if SBS[j][i]==1:
+				CP_count+=1
+		Nsbs[0][i] = EP_count
+		Nsbs[1][i] = CP_count
+	return Nsbs
+
+def N(cust):
+	EP_count = Nsbs[0][cust]
+	CP_count = Nsbs[1][cust]
+	return EP_count+CP_count
+
+def P(cp_i, c_j):
+	P=0
+	if SBS[cp_i, c_j]==1:
+		P = 1 / N[EP, CP, c_j]
+	else:
+		pass
+	return P
+
+def E(kCP, C):
+	E = 0
+	for cp_i in kCP:
+		for c_j in C:
+			E += P(cp_i, c_j)
+	return E
+
+def Espga(S, C):
+	E = 0
+	for cj in C:
+		E+=P(S, cj)
+	return E
+
+def SPGA(k):
+	k = [0]*k
+	heapq.heapify(k)
+	Nsbs = N_Calc()
+	for cp in CP:
+		S = cp
+		E = Espga(S, C)
+		heapq.heappush(k, E)
+		heapq.heappop(k)
+	return k
+
+
+try:
+	SBS = read_SBS("data_process/car_SBS.csv")
+except:
+	SBS = create_SBS("car/car.data")
+SBS = np.array(SBS, dtype=np.uint8)
+np.random.shuffle(SBS)
+
+h=len(SBS)
+w=len(SBS[0])
+
+C = np.arange(0, w)
+EP = np.arange(0, (h*30)//100)
+CP = np.arange((h*30)//100, h)
+# kCP = ?
+
+k = SPGA(5)
+print(k)
